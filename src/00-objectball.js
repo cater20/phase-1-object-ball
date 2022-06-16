dd8e6620b2199e55eb082d62dbc2bcd641936cb9
cater20 <onyangoketta@gmail.com>
2022-06-16 08:25:07 +0300

    done with assignment

---
+++ src/00-objectball.js
@@ -0,0 +1,151 @@
+
+  
+ function gameObject()
+
+{
+  return{
+  home: {
+    "teamName":"Brooklyn Nets",
+    "colors": "Black, White",
+    "players":{
+      "Alan Anderson": {
+        Number:0,
+        shoe:16,
+        points:22,
+        rebounds:12, 
+        assists:12,
+        steals:3,
+        blocks:1,
+        slamsDuck:1},
+      "Reggie Evans":{
+        Number:30,
+        shoe:14,
+        points:12,
+        rebounds:12,
+        assists:12,
+        steals:12,
+        blocks:12,
+        slamsDuck:7},
+      "Brook Lopez":{
+        Number:11,
+        shoe:17,
+        points:17,
+        rebounds:19,
+        assists:10,
+        steals:3,
+        bocks:1,
+        slamsDuck:15},
+      "Mason plumlee": {
+        Number:1,
+        shoe:19,
+        points:26,
+        rebounds:12,
+        assists:6,
+        steals:3,
+        blocks:8,
+        slamsDuck:5},
+      "Jason Terry": {
+        Number:31,
+        shoe:15,
+        points:19,
+        rebounds:2,
+        assists:2,
+        steals:4,
+        blocks:11,
+        slamsDuck:1},
+    
+    
+    }
+    
+  },
+  away: {
+    "teamName": "Charlottee Hornets",
+    "colors": "Turquoise, purple",
+    "players":{
+      "Jeff Adrien":{
+        Number:4,
+        shoe:18,
+        points:10,
+        rebounds:1,
+        assists:1,
+        steals:2,
+        blocks:7,
+        slamsDuck:2},
+      "Bismak Biyombo":{
+        Number:0,
+        shoe:16,
+        points:12,
+        rebounds:4,
+        assists:7,
+        steals:7,
+        blocks:15,
+        slamsDuck:10},
+      "DesSgna Diop":{
+        Number:2,
+        shoe:14,
+        points:24,
+        rebounds:12,
+        assists:12,
+        steals:4,
+        blocks:5,
+        slamsDuck:5},
+      "Ben Gordon": {
+        Number:8,
+        shoe:15,
+        points:33,
+        rebounds:3,
+        assists:2,
+        steals:1,
+        blocks:1,
+        slamsDuck:0},
+      "Brendan Hayhood": {
+        Number:33,
+        shoe:15,
+        points:6,
+        rebounds:2,
+        assists:12,
+        steals:22,
+        blocks:5,
+        slamsDuck:12},
+    
+  } ,
+}}
+}
+function homeTeamName() {
+  let object = gameObject()
+  return object['home']['teamName']}
+
+debugger
+
+function goodPractices() {
+  let game = gameObject();
+  for (let gameKey in game) {
+    // are you ABSOLUTELY SURE what 'gameKey' is?
+    // use the debugger to find out!
+    debugger
+    let teamObj = game[gameKey]
+    for (let teamKey in teamObj) {
+      // are you ABSOLUTELY SURE what 'teamKey' is?
+      // use debugger to find out!
+      debugger
+
+      // what is 'data' at each loop through out this block?
+      // when will the following line of code work and when will it break?
+      let data = teamObj.player
+      for (let key in data) {
+        debugger
+      }
+    }
+  }
+}
+
+// then, call the function so it runs!
+goodPractices()
+
+function numPointsScored(playerInput) {
+  return players[playerInput].points
+}
+
+
+
+debugger
